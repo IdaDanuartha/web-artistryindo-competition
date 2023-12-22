9eae1d022a7cacf504e727bec2dc8a551319bdc0
IdaDanuartha <danuart21@gmail.com>
2023-12-22 20:10:11 +0800

    add page artistry

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import NavbarComponent from '../components/NavbarComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
 import { ref } from "vue";
 
 let active = ref(false);
@@ -15,6 +16,7 @@ const activeSidebar = () => {
     <NavbarComponent :active="active" @active-sidebar="activeSidebar"></NavbarComponent>
   </div>
   <router-view/>
+  <FooterComponent></FooterComponent>
 </template>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ const routes = [
     component: () => import('../layouts/HomeLayout.vue'),
     children: [
       {path: '/', name: 'home', component: () => import('../views/HomeView.vue')},
+      {path: '/artistry', name: 'artistry', component: () => import('../views/ArtistryView.vue')},
     ]
   },
 ]
