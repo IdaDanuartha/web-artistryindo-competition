54f480bad9237a36fd8a08c62c514816ea443e4f
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-22 22:05:45 +0800

    Progress: Update

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,10 @@ const routes = [
     component: () => import('../layouts/HomeLayout.vue'),
     children: [
       {path: '/', name: 'home', component: () => import('../views/HomeView.vue')},
-      {path: '/artistry', name: 'artistry', component: () => import('../views/ArtistryView.vue')},
-    ]
-  },
-  {
-    path: '/about',
-    component: () => import('../layouts/AboutLayout.vue'),
-    children: [
       {path: '/about', name: 'about', component: () => import('../views/AboutView.vue')},
+      // {path: '/categories-artistry', name: 'categories-artistry', component: () => import('../views/CategoryView.vue')},
+      {path: '/categories-artistry', name: 'categories-artistry', component: () => import('../views/CategoryView.vue')},
+      {path: '/artistry', name: 'artistry', component: () => import('../views/ArtistryView.vue')},
     ]
   },
 ]
